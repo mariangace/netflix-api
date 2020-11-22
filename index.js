2246fae9d070857c4441ba7e8080677fa10c5bfe
Mariana García <mariangace@gmail.com>
2020-11-22 17:19:42 -0600

    adding login and register endpoint

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+const { Schema } = mongoose; //Grab the schema from mongoose
+require('dotenv').config()
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kvegb.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`, 
+{ useCreateIndex: true, 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true});
+
+const User = mongoose.model('Users', new Schema({
+    name: String,
+    email: { type: String,
+        required: true,
+        unique: true },
+    password: {type: String, required:true}
+  }));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World Naz!')
+  res.send('Hello World Mar!')
 })
 
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        name:req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    newUser.save((err, user)=>{
+        console.log("all is user")
+        console.log(user)
+        if(err){
+            res.status(400).send({
+                status: err
+            })
+        }
+        res.status(200).send("registered");
+    })
+})
+
+app.post('/login', (req, res) => {
+    const { password, email}  = req.body; 
+    User.findOne({ email, password }, (err, user) => {
+        if(user){
+            res.status(200).send({
+                status: "valid"
+            }) 
+        }else{
+           
+            res.status(400).send({
+                status:"Not Found"
+            })
+        }
+
+    })
+    
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
