a431d3885b45251b092be400694fdcc3b0d0f838
Mariana García <mariangace@gmail.com>
2020-11-29 19:22:56 -0600

    adding content header request and fixing cors problem

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const { Schema } = mongoose; //Grab the schema from mongoose
+var cors = require('cors');
 require('dotenv').config()
+
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kvegb.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`, 
 { useCreateIndex: true, 
@@ -17,6 +19,13 @@ const User = mongoose.model('Users', new Schema({
         unique: true },
     password: {type: String, required:true}
   }));
+
+app.use(cors());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
 
 app.use(express.json());
 
@@ -44,6 +53,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { password, email}  = req.body; 
+    console.log(req.body)
     User.findOne({ email, password }, (err, user) => {
         if(user){
             res.status(200).send({
